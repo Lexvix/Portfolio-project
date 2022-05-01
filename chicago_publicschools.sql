6033834bd9ecadc31d887673b5b58273625afd0e
Lex YAN <101482618+Lexvix@users.noreply.github.com>
2022-05-01 12:07:13 +1000

    Add files via upload

---
+++ chicago_publicschools.sql
@@ -0,0 +1,67 @@
+SELECT * FROM portfolio.chicagopublicschools;
+
+select count(*) from portfolio.chicagopublicschools
+where Elementary_Middle_High_School = 'ES';
+
+select max(SAFETY_SCORE)
+as max_safety_score
+from portfolio.chicagopublicschools;
+
+select  NAME_OF_SCHOOL, SAFETY_SCORE from portfolio.chicagopublicschools
+where SAFETY_SCORE = (select max(SAFETY_SCORE) from portfolio.chicagopublicschools);
+
+select NAME_OF_SCHOOL, AVERAGE_STUDENT_ATTENDANCE
+from portfolio.chicagopublicschools
+order by AVERAGE_STUDENT_ATTENDANCE desc
+limit 10;
+
+select NAME_OF_SCHOOL, AVERAGE_STUDENT_ATTENDANCE
+from portfolio.chicagopublicschools
+where AVERAGE_STUDENT_ATTENDANCE <>''
+order by AVERAGE_STUDENT_ATTENDANCE asc
+limit 10; 
+
+select NAME_OF_SCHOOL, AVERAGE_STUDENT_ATTENDANCE
+from portfolio.chicagopublicschools
+where 
+AVERAGE_STUDENT_ATTENDANCE <>'' and
+AVERAGE_STUDENT_ATTENDANCE < '70%';
+
+
+select COMMUNITY_AREA_NAME, 
+sum(COLLEGE_ENROLLMENT) as TOTAL_ENROLLMENTS
+from portfolio.chicagopublicschools
+group by COMMUNITY_AREA_NAME
+order by TOTAL_ENROLLMENTS desc;
+
+
+select COMMUNITY_AREA_NAME, 
+sum(COLLEGE_ENROLLMENT) as TOTAL_ENROLLMENTS
+from portfolio.chicagopublicschools
+group by COMMUNITY_AREA_NAME
+order by TOTAL_ENROLLMENTS asc
+limit 5;
+
+
+select NAME_OF_SCHOOL, SAFETY_SCORE
+from portfolio.chicagopublicschools
+order by SAFETY_SCORE asc
+limit 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
